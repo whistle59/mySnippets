87e20dd5770fd309dc10a8a5b1037b809cd1aca8
whistle59 <3628770+whistle59@users.noreply.github.com>
2022-12-08 12:49:54 +0100

    Create basic_callback_examples.py

---
+++ python/callbacks/basic_callback_examples.py
@@ -0,0 +1,11 @@
+def callbackPrint(s):
+    print('*** - Length of the text file is: ', s)
+    
+def findFileLength(path, callback):
+    f = open(path, "r")
+    length = len(f.read())
+    f.close()
+    callback(length)
+    
+if __name__ == '__main__':
+    findFileLength("/Users/miguel.rooney/Jon.txt",callbackPrint)
