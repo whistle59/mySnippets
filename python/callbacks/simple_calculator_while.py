9f882ff5cd39239c26d721e5909c8ea681e0e67a
whistle59 <3628770+whistle59@users.noreply.github.com>
2022-12-08 12:53:33 +0100

    Create simple_calculator_while.py

---
+++ python/callbacks/simple_calculator_while.py
@@ -0,0 +1,36 @@
+def add(a, b):
+    return a + b
+
+def subs(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+def calculate(operation):
+    if operation==1:
+        return add
+    elif operation==2:
+        return subs
+    elif operation==3:
+        return mult
+
+while True:
+    print('Arithmetic operations')
+    print('1.- addition')
+    print('2.- substraction')
+    print('3.- multiplication')
+    print('0.- exit')
+    operation = int(input('Select operation: '))
+    if (operation not in [0, 1, 2, 3]):
+        print("wrong number")
+        break
+    if operation==0:
+        break
+    func = calculate(operation)
+    print('***')
+    a = int(input('first operand'))
+    b = int(input('second operand'))
+    result = func(a, b)
+    print('***')
+    print('The result is: ', result)
